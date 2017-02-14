8b6741799f5aec5b70dd137f295ac9b04afe039a
José Antonio Martínez <jamartinez@enxine.com>
2017-02-14 19:16:37 +0100

    Updated example eth-blu-scanner.js: fixed bugs and adapted so that it can be runned in RPi-3B and also in RPi (without bluetooth)

--- eth-blu-scanner.js
+++ eth-blu-scanner.js
@@ -1,15 +1,44 @@
+// (c) Copyright 2017 Enxine DEV S.C.
+// Released under Apache License, version 2.0
+// @author: jamartinez@enxine.com
+//
+// It works as a "who-is-in" or presence detector
+// It search for nearby devices using both interfaces: ethernet and bluetooth.
+// Ethernet searches are based in ARP requests.
+//
+// See details about how to configure environment an run the example here:
+//   https://developers.vizibles.com/en/devices/raspberry/
+//
+
 // devDependency (only for debugging)
 //var logtimestamp = require('log-timestamp');
+
+var debug = false;
+var LOG = debug ? console.log.bind(console) : function () {};
 
 var fs = require('fs');
 var browser = require('iotdb-arp');
 var vizibles = require('vizibles');
-var cloudConnectionOpened = false;
+
+var connected = false;
 var scanInterval = null;
 
+// If set to true only previously know devices are considered. In other case
+// all devices found are updated in the platform.
 var ONLY_KNOWN_DEVICES = true;
 
+// Frequency of searching loop execution
+var SEARCH_FREQ = 20000;
+
 var knownDevices = {
+    /*
+      This object defines a list with your known devices. Each property name
+      is the MAC address of a device, and its value is an object containing
+      the name that you can give to that device in the platform. For example:
+
+      '00-00-00-00-00-01': {'name': 'DEVICE01-NAME'},
+      '00-00-00-00-00-02': {'name': 'DEVICE02-NAME'}
+    */
 };
 
 var bluFoundDevices = [];
@@ -34,7 +63,7 @@ function clearAllConnectionStatus() {
 	    prop[knownDevices[property].name] = 0;
 	}
     }
-    console.log('Update: ' + JSON.stringify(prop));
+    LOG('Update: ' + JSON.stringify(prop));
     vizibles.update(prop);
 }
 
@@ -49,7 +78,7 @@ function updateDeviceStatus(mac, status) {
     prop[deviceName] = status;
 
     if (isKnown || !ONLY_KNOWN_DEVICES) {
-	console.log('Update: ' + JSON.stringify(prop));
+	LOG('Update: ' + JSON.stringify(prop));
 	vizibles.update(prop);
     }
 }
@@ -57,11 +86,14 @@ function updateDeviceStatus(mac, status) {
 function scanEth() {
     ethFoundDevices.splice(0);
     browser.browser({}, function(error, packet) {
-	if (error) return;
+	if (error) {
+	    LOG('Error: ');
+	    LOG(error);
+	    return;
+	}
 	else if (packet) {
-	    //console.log('arp found: ' + packet);
 	    var normMac = packet.mac.replace(/\W/g, '-');
-	    console.log('Found MAC: ' + normMac);
+	    LOG('Found MAC: ' + normMac);
 	    if (ethFoundDevices.indexOf(normMac) == -1) ethFoundDevices.push(normMac);
 	    if (ethCurrentDevices.indexOf(normMac) == -1) {
 		ethCurrentDevices.push(normMac);
@@ -76,95 +108,84 @@ function checkAddress(device) {
     return false;
 }
 
-function checkFoundLastSearch(device) {
-    if (device.foundLastSearch) return true;
-    return false;
-}
-
-function checkFound1Times(device) {
-    if (device.counter >= 1) return true;
-    return false;
-}
-
-function checkFoundPending(device) {
-    if (device.counter < 1) return true;
-    return false;
-}
-
-function checkDelCounterNotReached(device) {
-    if (device.delCounter < 3) return true;
-    return false;
-}
-
 function onConnected() {
-    console.log('Connected to Vizibles!');
-    if (!cloudConnectionOpened) {
-        cloudConnectionOpened = true;
+    LOG('Connected to Vizibles!');
+    if (!connected) {
+        connected = true;
 
 	clearAllConnectionStatus();
 
-	var btSerial = new (require('bluetooth-serial-port')).BluetoothSerialPort();
-
-	btSerial.on('found', function(address, name) {
-	    //console.log('Found: ' + address + ', ' + name);
-	    console.log('Found BLU: ' + address);
-	    var foundDevice = bluFoundDevices.find(checkAddress, address);
-	    if (foundDevice) {
-		foundDevice.foundLastSearch = true;
-		foundDevice.counter++;
-	    } else {
-		var newDevice = {};
-		newDevice.address = address;
-		newDevice.name = name;
-		newDevice.foundLastSearch = true;
-		newDevice.counter = 1;
-		newDevice.delCounter = 0;
-		bluFoundDevices.push(newDevice);
-	    }
-
-	    var currentDevice = bluCurrentDevices.find(checkAddress, address);
-	    if (currentDevice) {
-		currentDevice.foundLastSearch = true;
-	    }
-	    
-	});
-	
-	btSerial.on('finished', function() {
-	    //console.log('Finished');
-	    bluFoundDevices = bluFoundDevices.filter(checkFoundLastSearch);
-	    var newDevices = bluFoundDevices.filter(checkFound1Times);
-	    //console.log('found: ' + JSON.stringify(bluFoundDevices));
-	    //console.log('new: ' + JSON.stringify(newDevices));
-	    for (i = 0; i < newDevices.length; i++) {
-		if (!bluCurrentDevices.find(checkAddress, newDevices[i].address)) {
-		    bluCurrentDevices.push(newDevices[i]);
-		    updateDeviceStatus(newDevices[i].address.replace(/\W/g, '-'), 1);
-		}
-	    }
-	    bluFoundDevices = bluFoundDevices.filter(checkFoundPending);
-
-	    for (i = 0; i < bluCurrentDevices.length; i++) {
-		if (bluCurrentDevices[i].foundLastSearch) {
-		    bluCurrentDevices[i].delCounter = 0; 
+	var bluetoothSerialPort = null;
+	try {
+	    bluetoothSerialPort = require('bluetooth-serial-port');
+	}
+	catch (e) {}
+	if (bluetoothSerialPort) {
+	    var btSerial = new (bluetoothSerialPort).BluetoothSerialPort();
+	    btSerial.on('found', function(address, name) {
+		LOG('Found BLU: ' + address);
+		var foundDevice = bluFoundDevices.find(checkAddress, address);
+		if (foundDevice) {
+		    foundDevice.foundLastSearch = true;
+		    foundDevice.counter++;
 		} else {
-		    bluCurrentDevices[i].delCounter++;
-		    if (bluCurrentDevices[i].delCounter >= 3) {
-			updateDeviceStatus(bluCurrentDevices[i].address.replace(/\W/g, '-'), 0);
+		    var newDevice = {};
+		    newDevice.address = address;
+		    newDevice.name = name;
+		    newDevice.foundLastSearch = true;
+		    newDevice.counter = 1;
+		    newDevice.delCounter = 0;
+		    bluFoundDevices.push(newDevice);
+		}
+
+		var currentDevice = bluCurrentDevices.find(checkAddress, address);
+		if (currentDevice) {
+		    currentDevice.foundLastSearch = true;
+		}
+		
+	    });
+	    
+	    btSerial.on('finished', function() {
+		bluFoundDevices = bluFoundDevices.filter(function(device) { return device.foundLastSearch });
+		var newDevices = bluFoundDevices.filter(function(device) { return (device.counter >= 1) });
+		for (i = 0; i < newDevices.length; i++) {
+		    if (!bluCurrentDevices.find(checkAddress, newDevices[i].address)) {
+			bluCurrentDevices.push(newDevices[i]);
+			updateDeviceStatus(newDevices[i].address.replace(/\W/g, '-'), 1);
 		    }
 		}
-	    }
-	    bluCurrentDevices = bluCurrentDevices.filter(checkDelCounterNotReached);
-	});
+		bluFoundDevices = bluFoundDevices.filter(function(device) { return (device.counter < 1) });
 
+		for (i = 0; i < bluCurrentDevices.length; i++) {
+		    if (bluCurrentDevices[i].foundLastSearch) {
+			bluCurrentDevices[i].delCounter = 0; 
+		    } else {
+			bluCurrentDevices[i].delCounter++;
+			if (bluCurrentDevices[i].delCounter >= 3) {
+			    updateDeviceStatus(bluCurrentDevices[i].address.replace(/\W/g, '-'), 0);
+			}
+		    }
+		}
+		bluCurrentDevices = bluCurrentDevices.filter(function(device) { return (device.delCounter < 3) });
+	    });
+	}
 	
 	scanInterval = setInterval(function() {
-	    console.log('\n\nSearching....');
-	    // remove devices not found in last 5 searches
+	    LOG('\n\nSearching....');
+
+	    // restart count for devices found
+	    ethFoundDevices.forEach(function(item) {
+		if (ethNotFoundDevices.hasOwnProperty(item)) {
+		    delete ethNotFoundDevices[item];
+		}
+	    });
+	    
+	    // remove devices not found in last 20 searches
 	    var notFoundDevices = ethCurrentDevices.filter(function(device) { return ethFoundDevices.indexOf(device) == -1 });
 	    notFoundDevices.forEach(function(item) {
 		if (ethNotFoundDevices.hasOwnProperty(item)) {
 		    ethNotFoundDevices[item]++;
-		    if (ethNotFoundDevices[item] == 5) {
+		    if (ethNotFoundDevices[item] == 20) {
 			ethCurrentDevices.splice(ethCurrentDevices.indexOf(item), 1);
 			updateDeviceStatus(item, 0);
 			delete ethNotFoundDevices[item];
@@ -183,21 +204,25 @@ function onConnected() {
 	    for (i = 0; i < bluCurrentDevices.length; i++) {
 		bluCurrentDevices[i].foundLastSearch = false;
 	    }
-	    //console.log('initial found: ' + JSON.stringify(bluFoundDevices));
-	    btSerial.inquire();
-        }, 15000);      
+	    
+	    if (bluetoothSerialPort) btSerial.inquire();
+	    
+        }, SEARCH_FREQ);      
 
     }
 }
 
 function onDisconnected(err) {
-    console.log('Disconnected from Vizibles with error: ' + JSON.stringify(err));
+    LOG('Disconnected from Vizibles with error: ' + JSON.stringify(err));
     clearInterval(scanInterval);
-    cloudConnectionOpened = false;
+    connected = false;
 }
 
 vizibles.connect({
     id: 'eth-blu-scanner',
-    credentials: {keyId: '<TO_BE_COMPLETED>', secret: '<TO_BE_COMPLETED>'},
+    // TODO: replace the <TODO> strings with values obtained from Vizibles and
+    // then uncomment next line
+    //credentials: {keyId: '<TODO>', secret: '<TODO>'}, 
     onConnected: onConnected, 
-    onDisconnected: onDisconnected});
+    onDisconnected: onDisconnected
+});
